cb74d8fb4e8ba7b0960a2cbbccfd70b6fd90418e
krkart <krkart@zohomail.in>
2025-03-19 19:20:33 +0530

    basic sign in/up design updated

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ let mylogin = document.getElementById('login');
 let myreg = document.getElementById('register');
 let mybtn = document.getElementById('btn');
 
-login = () =>{
+register = () => {
     mylogin.style.left = '50px';
     myreg.style.left = '500px';
     mybtn.style.left = '0px';
 }
 
-register = () =>{
+login = () => {
     mylogin.style.left = '-450px';
     myreg.style.left = '50px';
     mybtn.style.left = '110px';
