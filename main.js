9f34c12b35f631ccd0eeeb13a3038b6762e1b893
krkart <krkart@zohomail.in>
2025-03-20 11:19:06 +0530

    bg header design updated

--- main.js
+++ main.js
@@ -5,13 +5,13 @@ let myreg = document.getElementById('register');
 let mybtn = document.getElementById('btn');
 
 register = () => {
-    mylogin.style.left = '50px';
-    myreg.style.left = '500px';
-    mybtn.style.left = '0px';
+  mylogin.style.left = '50px';
+  myreg.style.left = '500px';
+  mybtn.style.left = '0px';
 }
 
 login = () => {
-    mylogin.style.left = '-450px';
-    myreg.style.left = '50px';
-    mybtn.style.left = '110px';
+  mylogin.style.left = '-450px';
+  myreg.style.left = '50px';
+  mybtn.style.left = '110px';
 }
