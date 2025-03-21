8a9ccb3f229da702460008734bc2a253d722f365
krkart <krkart@zohomail.in>
2025-03-21 15:25:48 +0530

    form fields design updated

--- main.js
+++ main.js
@@ -1,17 +1,45 @@
 
 
-let mylogin = document.getElementById('login');
 let myreg = document.getElementById('register');
+let mylogin = document.getElementById('login');
 let mybtn = document.getElementById('btn');
 
 register = () => {
-  mylogin.style.left = '50px';
-  myreg.style.left = '500px';
+  myreg.style.left = '50px';
+  mylogin.style.left = '500px';
   mybtn.style.left = '0px';
 }
 
 login = () => {
-  mylogin.style.left = '-450px';
-  myreg.style.left = '50px';
+  myreg.style.left = '-450px';
+  mylogin.style.left = '50px';
   mybtn.style.left = '110px';
 }
+
+const myform = document.querySelectorAll('.inputArea');
+forEach(field in myform).noValidate = true;
+
+const btns = document.querySelectorAll('.submit_btn')
+forEach(btn in btns).addEventListener('submit', validateForm);
+
+function validateForm(e) {
+  const form = e.target;
+  if (form.checkValidity()) {
+    // form is valid - make further checks
+  }
+  else {
+    // form is invalid - cancel submit
+    e.preventDefault();
+    // apply invalid class
+    Array.from(form.elements).forEach(i => {
+      if (i.checkValidity()) {
+        // field is valid - remove class
+        i.parentElement.classList.remove('invalid');
+      }
+      else {
+        // field is invalid - add class
+        i.parentElement.classList.add('invalid');
+      }
+    });
+  }
+};
